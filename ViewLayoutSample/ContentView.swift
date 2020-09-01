f9b6ff69a3c2404c57b9735a8ca36f3a6a03fe6c
Jeremiah <jjessel84@gmail.com>
2020-08-31 19:50:44 -0700

    Initial Commit

---
+++ ViewLayoutSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewLayoutSample
+//
+//  Created by Jeremiah Jessel on 8/31/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewLayoutSample/ViewLayoutSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewLayoutSampleApp.swift
+//  ViewLayoutSample
+//
+//  Created by Jeremiah Jessel on 8/31/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewLayoutSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
